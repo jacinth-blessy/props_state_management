2b88f09a17c4b67fcb7774ad68ea1a20846457f5
jacinth-blessy <jacinthmblessy2024@gmail.com>
2025-12-17 23:31:11 +0530

    added profile & info components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Counter from "./Counter";
 import Calculator from "./Calculator";
 import PassProps from "./PassProps";
+import UserProfile from "./UserProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <PassProps title="Hello World" message="This is a message passed as props." />
       <PassProps title="Another Title" message="Another message for the second component." />
       <PassProps title="Final Title" message="Final message in the last component." />
+      <UserProfile />
     </div>
   );
 }
